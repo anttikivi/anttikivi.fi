e59db7057c8d4a49b33ecd6b2c94a334a7798fa4
Antti Kivi <antti@anttikivi.fi>
2024-09-18 15:01:59 +0300

    build: add Prettier

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [
+    "prettier-plugin-packagejson",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  arrowParens: "always",
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  proseWrap: "always",
+};
+
+export default config;
