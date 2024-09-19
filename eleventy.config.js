94e0b3ae1a6528ade8b91983cdf28a9278ad9359
Antti Kivi <antti@anttikivi.fi>
2024-09-19 07:46:29 +0300

    build: add Tailwind and transpile CSS with PostCSS during the 11ty build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,20 @@
-/** @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig */
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+
+/**
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ */
 export default function (eleventyConfig) {
+  eleventyConfig.addTemplateFormats("css");
+  eleventyConfig.addExtension("css", {
+    outputFileExtension: "css",
+    compile: (inputContent) => {
+      return async () =>
+        (await postcss([tailwindcss, autoprefixer]).process(inputContent)).css;
+    },
+  });
+
   return {
     dir: {
       input: "src",
