d506499c5032c3e43382f98a9b8869ef305c7655
Antti Kivi <antti@anttikivi.fi>
2024-09-21 19:53:08 +0300

    feat: add the canonical URL and the correct alternative URLs

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,4 +1,10 @@
 export default {
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://www.anttikivi.fi"
+      : process.env.NODE_ENV === "staging"
+        ? "https://staging.anttikivi.fi"
+        : "http://localhost:8080",
   description: "Viestintäasiantuntija, yrittäjä ja ylioppilas",
   subtitle: "Viestinnän asiantuntija",
   title: "Antti Kivi",
