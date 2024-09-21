5edaecfb246721d1c3c844393ea55f1cdcbb2f14
Antti Kivi <antti@anttikivi.fi>
2024-09-21 11:48:07 +0300

    build: add the 11ty i18n plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+import EleventyI18nPlugin from "@11ty/eleventy";
 import autoprefixer from "autoprefixer";
 import postcss from "postcss";
 import tailwindcss from "tailwindcss";
@@ -6,6 +7,10 @@ import tailwindcss from "tailwindcss";
  * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
  */
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "fi",
+  });
+
   eleventyConfig.addTemplateFormats("css");
   eleventyConfig.addExtension("css", {
     outputFileExtension: "css",
