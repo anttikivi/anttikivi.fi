468e156d0ef61f9a2411ecfab357df91ae478233
Antti Kivi <antti@anttikivi.fi>
2024-10-29 08:35:49 +0200

    feat: add a basic CV page

--- src/_data/paths.js
+++ src/_data/paths.js
@@ -1,10 +1,12 @@
 export default {
   en: {
     "/": "/en/",
+    cv: "/en/cv/",
     "data-protection": "/en/data-protection/",
   },
   fi: {
     "/": "/",
+    cv: "/ansioluettelo/",
     "data-protection": "/tietosuoja/",
   },
 };
