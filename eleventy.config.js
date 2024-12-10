cf7edc4e015d1e704cd53144b89dc14637826a1f
Antti Kivi <antti@anttikivi.fi>
2024-12-10 22:14:41 +0200

    build: install Eleventy, Prettier, and ESLint

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,5 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
-export default function (eleventyConfig) {
-
-}
+export default function (eleventyConfig) {}
 
 export const config = {
   dir: {
