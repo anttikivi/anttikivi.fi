d07351c11a7b9c301e0d2cea0b7a1ba115f3a6da
Antti Kivi <antti@anttikivi.fi>
2024-12-10 21:38:29 +0200

    refactor!: remove the old code

--- src/_data/contact.js
+++
@@ -1,7 +0,0 @@
-export default {
-  socialMedia: {
-    githubUrl: "https://github.com/anttikivi",
-    instagramUrl: "https://www.instagram.com/anttikiwi/",
-    threadsUrl: "https://www.threads.net/@anttikiwi",
-  },
-};
