d6729d84e83a662bf85eff28b1d771533f9376e8
Antti Kivi <antti@anttikivi.fi>
2024-12-14 00:56:00 +0200

    feat: add CSS compiler options for the different builds

--- utils/css.js
+++ utils/css.js
@@ -1,8 +1,48 @@
+import browserslist from "browserslist";
+import { browserslistToTargets, Features } from "lightningcss";
 import memoize from "memoize";
 import crypto from "node:crypto";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { compile } from "tailwindcss-node-compiler";
+import fixTailwindColors from "./lightningcss-plugin-fix-tailwind-colors/index.js";
+
+/**
+ * @param {"development" | "staging" | "production"} env The value of NODE_ENV.
+ *
+ * @returns {import("tailwindcss-node-compiler").CompilerOptions}
+ */
+function _createOptions(env) {
+  const defaultOptions = {
+    sourceMap: false,
+    include: Features.Nesting,
+    exclude: Features.LogicalProperties | Features.DirSelector,
+    visitor: fixTailwindColors,
+  };
+
+  if (env === "production") {
+    const targets = browserslistToTargets(
+      browserslist(">= 0.005% and not dead"),
+    );
+    return Object.assign({}, defaultOptions, {
+      minify: true,
+      targets,
+    });
+  }
+
+  if (env === "staging") {
+    const targets = browserslistToTargets(browserslist("defaults"));
+    return Object.assign({}, defaultOptions, {
+      minify: false,
+      targets,
+    });
+  }
+
+  return { disableTransforms: true };
+}
+
+/** @type {typeof _createOptions} */
+const createOptions = memoize(_createOptions);
 
 /**
  * @param {string} input The CSS that should be compiled.
@@ -13,7 +53,7 @@ async function _processCSS(input) {
   const code = await compile(
     input,
     path.resolve(process.cwd(), "src"),
-    undefined,
+    createOptions(process.env.NODE_ENV),
   );
   const hash = crypto.createHash("sha256").update(code).digest("hex");
 
