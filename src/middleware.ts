5f8afe459595d4031ace01dcc0256841d36de2c8
Antti Kivi <antti@anttikivi.fi>
2025-03-07 15:31:12 +0200

    refactor!: clean up the code

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { routing } from "@/i18n/routing";
-import createMiddleware from "next-intl/middleware";
-
-export default createMiddleware(routing);
-
-export const config = {
-  matcher: ["/", "/(de|en)/:path*", "/((?!api|_next|_vercel|.*\\..*).*)"],
-};
