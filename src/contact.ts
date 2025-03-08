8a1de6406ba87105fe28235bc20ca643efccf1a7
Antti Kivi <antti@anttikivi.fi>
2025-03-08 23:19:21 +0200

    feat: add home page and social media links to the footer

---
+++ src/contact.ts
@@ -0,0 +1,7 @@
+const contact = {
+  githubURL: "https://github.com/anttikivi",
+  instagramURL: "https://www.instagram.com/anttikiwi/",
+  threadsURL: "https://www.threads.net/@anttikiwi",
+};
+
+export default contact;
