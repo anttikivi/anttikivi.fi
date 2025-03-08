7e91082f51529032f6008ecfa361ee8474007b10
Antti Kivi <antti@anttikivi.fi>
2025-03-08 09:12:19 +0200

    build: add the base URL for the site

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,26 @@ import type { Locales } from "astro";
 import { defineConfig } from "astro/config";
 import locales, { defaultLocale } from "./src/locales";
 
+function getSiteURL() {
+  // https://docs.astro.build/en/guides/environment-variables/
+  if (import.meta.env.PROD) {
+    return "https://www.anttikivi.fi";
+  }
+
+  return "http://localhost:4321";
+}
+
 // https://astro.build/config
 export default defineConfig({
+  site: getSiteURL(),
   trailingSlash: "always",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
+  },
+  build: {
+    format: "directory",
+    assets: "_static",
   },
   i18n: {
     locales: locales as unknown as Locales,
