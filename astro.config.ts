b9f08e81df6e37023895804805e90039ca9780b5
Antti Kivi <antti@anttikivi.fi>
2025-03-09 13:23:19 +0200

    feat: add `nofollow` to external links and add the ability to disable it for select links

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import type { Locales } from "astro";
 import { defineConfig } from "astro/config";
 import browserslist from "browserslist";
+import type { Element, Root, RootContent } from "hast";
 import { browserslistToTargets, Features } from "lightningcss";
+import rehypeAttr from "rehype-attr";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeMinifyWhitespace from "rehype-minify-whitespace";
+import rehypeRaw from "rehype-raw";
+import rehypeRemoveComments from "rehype-remove-comments";
+import rehypeRewrite from "rehype-rewrite";
 import locales, { defaultLocale } from "./src/locales";
 
 function getSiteURL() {
@@ -19,6 +26,7 @@ export default defineConfig({
   site: getSiteURL(),
   trailingSlash: "always",
   output: "static",
+  compressHTML: import.meta.env.PROD,
   vite: {
     build: {
       cssMinify: "lightningcss",
@@ -35,6 +43,43 @@ export default defineConfig({
   build: {
     format: "directory",
     assets: "_static",
+  },
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeAttr,
+        {
+          properties: "attr",
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["nofollow"],
+          test(element: Element) {
+            if ("data-skip" in element.properties) {
+              return false;
+            }
+
+            return true;
+          },
+        },
+      ],
+      [
+        rehypeRewrite,
+        {
+          rewrite(node: Root | RootContent) {
+            if (node.type === "element" && "data-skip" in node.properties) {
+              delete node.properties["data-skip"];
+            }
+          },
+        },
+      ],
+      rehypeRaw,
+      ...(import.meta.env.PROD
+        ? [rehypeRemoveComments, rehypeMinifyWhitespace]
+        : []),
+    ],
   },
   i18n: {
     locales: locales as unknown as Locales,
--- src/content/home/preface-fi.md
+++ src/content/home/preface-fi.md
@@ -3,7 +3,9 @@
 
 Hei, olen _Antti Kivi_.
 
+<!-- this is comment -->
+
 Olen [Suomen ylioppilaskuntien liiton](https://syl.fi) viestintäasiantuntija,
-[Visioston](https://www.visiosto.fi) toimitusjohtaja sekä valtiotieteiden ja
-luonnontieteiden ylioppilas Helsingin yliopistossa ja hallintotieteiden
-ylioppilas Tampereen yliopistossa.
+[Visioston](https://www.visiosto.fi)<!--rehype:data-skip=true--> toimitusjohtaja
+sekä valtiotieteiden ja luonnontieteiden ylioppilas Helsingin yliopistossa ja
+hallintotieteiden ylioppilas Tampereen yliopistossa.
